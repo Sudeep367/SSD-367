a4426ec9451b6a67e667b7052627438174128568
Sudeep367 <83811433+Sudeep367@users.noreply.github.com>
2021-05-07 14:12:16 +0530

    Add files via upload

---
+++ js/scripts.js
@@ -0,0 +1,59 @@
+$(document).ready(function() {
+  $(".menu-toggle").on("click", function() {
+   $(".nav").toggleClass("showing");
+  $ (".nav.ul").toggleClass("showing");
+  });
+
+  $(".post-wrapper").slick({
+    slidesToShow: 3,
+    slidesToScroll: 1,
+    autoplay: true,
+    autoplaySpeed: 2000,
+    nextArrow: $(".next"),
+    prevArrow: $(".prev"),
+    responsive: [
+      {
+        breakpoint:1024,
+        settings: {
+          slidesToShow: 3,
+          slidesToScroll: 3,
+          infinite: true,
+          dots: true
+        }
+      },
+    
+      // You can unslick at a given breakpoint now by adding:
+      // settings: "unslick"
+      // instead of a settings object
+    ]
+  });
+});
+
+ClassicEditor.create(document.querySelector("#body"), {
+  toolbar: [
+    "heading",
+    "|",
+    "bold",
+    "italic",
+    "link",
+    "bulletedList",
+    "numberedList",
+    "blockQuote"
+  ],
+  heading: {
+    options: [
+      { model: "paragraph", title: "Paragraph", class: "ck-heading_paragraph" },
+      {
+      
+      },
+      {
+        model: "heading2",
+        view: "h2",
+        title: "Heading 2",
+        class: "ck-heading_heading2"
+      }
+    ]
+  }
+}).catch(error => {
+  console.log(error);
+});
